074a8019a618afe900d7313c3f5643ffc7383c37
Herve Senot <hsenot@gmail.com>
2013-07-09 23:27:01 +1000

    Updated data schema (removed source column)

--- data/dump/dump.sh
+++ data/dump/dump.sh
@@ -1,3 +1,4 @@
+rm /opt/opengeo/suite/webapps/solarsize/data/dump/community.zip
 pg_dump -p 54321 -n community solar > /opt/opengeo/suite/webapps/solarsize/data/dump/community.sql
 zip /opt/opengeo/suite/webapps/solarsize/data/dump/community.zip /opt/opengeo/suite/webapps/solarsize/data/dump/community.sql
 rm /opt/opengeo/suite/webapps/solarsize/data/dump/community.sql
