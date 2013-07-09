623ce19a58792d58334e967ce52e6f35abb478ac
Herve Senot <hsenot@gmail.com>
2013-07-09 22:15:24 +1000

    Schema change for the building table

--- ws/database.inc.php
+++ ws/database.inc.php
@@ -3,7 +3,7 @@
 	function pgConnection() {
 		try {
 			# Connect to PostgreSQL database
-			$conn = pg_connect("dbname='bip' user='opengeo' password='opengeo' host='localhost' port='54321'");
+			$conn = pg_connect("dbname='solar' user='opengeo' password='opengeo' host='localhost' port='54321'");
 			return $conn;
 		}
 		catch (Exception $e) {
